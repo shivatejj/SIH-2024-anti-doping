97d58033410f27c9206d9da74f4f79101ccc0fbd
nitesh <nitesh.s@e-commsys.com.au>
2025-02-08 15:49:28 +0530

    main page changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
-import Login from "@/components/Login/Login";
+import dynamic from "next/dynamic";
+
+const Login = dynamic(
+  () => import("../components/Login/Login"),
+  { ssr: false, loading: () => <p>Loading...</p> }
+)
+
 
 export default function Home() {
   return (
