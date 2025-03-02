2c697657a459bc0ab0d7b20c5795e711c4622f67
nitesh <nitesh.s@e-commsys.com.au>
2025-03-02 18:01:29 +0530

    get quiz implemented

---
+++ src/pages/api/quiz/get.ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectDB } from "@/lib/mongodb";
+import { isAuthenticated } from "@/middleware/auth";
+import Quiz from "@/models/Quiz";
+import UserStats from "@/models/UserStats";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") return res.status(405).json({ message: "Method not allowed" });
+
+  await connectDB();
+
+  try {
+    const user = isAuthenticated(req);
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const { category, level } = req.query;
+
+    if (!category) {
+      return res.status(500).json({ message: "category is required" });
+    }
+
+    if (!level) {
+      return res.status(500).json({ message: "level is required" });
+    }
+
+    const quiz = await Quiz.findOne({ category: category, level: level });
+
+    if (!quiz) {
+      return res.status(404).json({ message: "Quiz not found" });
+    }
+
+    const shuffledQuestions = quiz.questions
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 5)
+      .map((data: { question: string, options: string[] }) =>
+        ({ question: data.question, options: data.options }));
+
+    if (user.role === 'user') {
+      await UserStats.findOneAndUpdate(
+        { userId: user.userId },
+        { $inc: { attempts: 1 } }
+      );
+    }
+
+    return res.status(200).json({ content: quiz.content, questions: shuffledQuestions });
+
+  } catch (error) {
+    if (typeof error === "object" && error !== null && "message" in error) {
+      const err = error as { message: string; status?: number };
+      return res.status(err.status || 500).json({ message: err.message });
+    }
+
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
