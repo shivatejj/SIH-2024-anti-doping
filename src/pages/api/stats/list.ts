831a6b08163e48599e2f7d911dcc33e562a26e93
nitesh <nitesh.s@e-commsys.com.au>
2025-03-16 17:44:09 +0530

    Leader board API implemented

--- src/pages/api/stats/list.ts
+++ src/pages/api/stats/list.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { connectDB } from "@/lib/mongodb";
 import { isAuthenticated } from "@/middleware/auth";
 import UserStats from "@/models/UserStats";
+import User from "@/models/User";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") return res.status(405).json({ message: "Method not allowed" });
 
@@ -9,22 +10,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { category } = req.query;
     await connectDB();
     const user = isAuthenticated(req);
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
+
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
 
     if (!category) return res.status(400).json({ message: 'Category is required' });
 
-    const searchQuery: Record<string, string> = { userId: user.userId }
+    const userStat = await UserStats.find({ category })
+      .sort({
+        score: -1,
+        attempts: 1,
+        updatedAt: 1,
+      })
+      .limit(10);
 
-    if (category) searchQuery.category = category as string;
-    const userStat = await UserStats.find(searchQuery);
+    if (!userStat) return res.status(404).json({ message: `No stats found for ${category} category` });
 
-    if (!userStat) {
-      return res.status(404).json({ message: 'No stats found' });
-    }
+    const result = await Promise.all(
+      userStat.map(async (stat: { userId: string; score: number }) => {
+        const userData = await User.findOne({ userId: stat.userId });
+        return {
+          userName: userData?.name,
+          score: stat.score,
+        };
+      })
+    );
 
-    return res.status(200).json({ userStat });
+    return res.status(200).json({ result });
 
   } catch (error) {
     if (typeof error === "object" && error !== null && "message" in error) {
